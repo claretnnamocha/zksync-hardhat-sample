ed958d867de7fc6a7f8bd359b02fde910922c6b3
claretnnamocha <devclareo@gmail.com>
2022-10-06 17:37:21 +0100

    config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,25 +6,15 @@ module.exports = {
     version: "1.1.5",
     compilerSource: "docker",
     settings: {
-      optimizer: {
-        enabled: true,
-      },
-      experimental: {
-        dockerImage: "matterlabs/zksolc",
-        tag: "v1.1.5",
-      },
+      // optimizer is always enabled
+      // optimizer: { enabled: true },
+      experimental: { dockerImage: "matterlabs/zksolc", tag: "v1.1.5" },
     },
   },
   zkSyncDeploy: {
     zkSyncNetwork: "https://zksync2-testnet.zksync.dev",
-    ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+    ethNetwork: "goerli",
   },
-  networks: {
-    hardhat: {
-      zksync: true,
-    },
-  },
-  solidity: {
-    version: "0.8.16",
-  },
+  networks: { hardhat: { zksync: true } },
+  solidity: { version: "0.8.16" },
 };
